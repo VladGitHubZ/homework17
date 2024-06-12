0c272f982fe842056579f61a30253678b9941d7c
Vladislav Zinziuk <2962266@mail.ru>
2024-06-12 20:03:15 +0300

    initial commit

---
+++ skypro/src/main/java/com/homework17/skypro/SkyproApplication.java
@@ -0,0 +1,13 @@
+package com.homework17.skypro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproApplication.class, args);
+	}
+
+}
